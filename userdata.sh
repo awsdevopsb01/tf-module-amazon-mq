acc58b9d0d1abb5c71cd8d6701ded8eacd359a4c
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-05-12 17:41:45 +0530

    rabbit mq changes

--- userdata.sh
+++ userdata.sh
@@ -3,4 +3,4 @@
 cd /opt
 git clone https://github.com/awsdevopsb01/roboshop-adv-shell.git
 cd roboshop-adv-shell
-bash rabbitmq.sh rabbitmq &>> /opt/roboshop.log
+bash rabbitmq.sh "rabbitmq" &>> /opt/roboshop.log
